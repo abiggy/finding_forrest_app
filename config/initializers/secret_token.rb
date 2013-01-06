bec845512dd4ec8277b45a601882c737c21fa0c2
Adam <adam.biglow@gmail.com>
2013-01-05 21:50:02 -0800

    Initial commit of finding forrest

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FindingForrestApp::Application.config.secret_token = '5818e329e882750cb0cff0482421064d4c682ac2962019c4a910638a971f813558a87ed69f2ca4ed4b7fd61c22ea6e5af1e366f3964b1a24486489f20bf89dc8'
